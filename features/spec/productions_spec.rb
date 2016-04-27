d95505ea4c9af55b6dc38a7a23fce83c423edcc2
xushanchuan <aisensiy@163.com>
2016-04-27 10:42:47 +0800

    update feature test title

--- features/spec/productions_spec.rb
+++ features/spec/productions_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../rails_helper'
 
 describe 'spec' do
-  it 'now we no longer need the full url' do
+  it 'can access production list' do
     get '/productions/index'
     expect_json([])
   end
